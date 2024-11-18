f7cafbdf9c394656d0038cff55985b133df172c5
EnricoToffalini <enrico.toffalini@unipd.it>
2024-11-18 14:05:56 +0100

    update

--- Slides/_rendering to pdf.R
+++ Slides/_rendering to pdf.R
@@ -1,7 +1,5 @@
-remotes::install_github("jhelvy/renderthis")
-
-renderthis::to_pdf("__NOME_DEL_FILE__.html")
-
+# remotes::install_github("jhelvy/renderthis")
+# renderthis::to_pdf("__NOME_DEL_FILE__.html")
 
 renderthis::to_pdf("0.GettingStartedR.html")
 renderthis::to_pdf("1.1.FirstStepsR-1.html")
@@ -9,4 +7,7 @@ renderthis::to_pdf("1.2.FirstStepsR-2.html")
 renderthis::to_pdf("2.DataStruct-1.html")
 renderthis::to_pdf("2.DataStruct-2.html")
 renderthis::to_pdf("3.Programming.html")
+
+
+
 
