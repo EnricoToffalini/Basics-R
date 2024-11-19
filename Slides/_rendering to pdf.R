a13ac26c8c41b58f718f229b70fb0182b5f2271a
EnricoToffalini <enrico.toffalini@unipd.it>
2024-11-19 14:24:27 +0100

    update

--- Slides/_rendering to pdf.R
+++ Slides/_rendering to pdf.R
@@ -4,9 +4,11 @@
 renderthis::to_pdf("0.GettingStartedR.html")
 renderthis::to_pdf("1.1.FirstStepsR-1.html")
 renderthis::to_pdf("1.2.FirstStepsR-2.html")
-renderthis::to_pdf("2.DataStruct-1.html")
-renderthis::to_pdf("2.DataStruct-2.html")
+renderthis::to_pdf("2.1.DataStruc-1.html")
+renderthis::to_pdf("2.2.DataStruc-2.html")
+renderthis::to_pdf("2.3.DataStruc-3.html")
 renderthis::to_pdf("3.Programming.html")
+
 
 
 
